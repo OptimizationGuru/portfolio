7c103f98eee5a84a34bba4f3fd5621b573d4a864
Shivam Tiwari <shivam.tiwari@yuma.energy>
2024-09-16 23:23:29 +0530

    toast added

--- src/components/contact/ContactRight.js
+++ src/components/contact/ContactRight.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
 import { fieldTypes } from '../../constants';
 import emailjs from 'emailjs-com';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const ContactRight = () => {
   const [finalFormError, setFinalFormError] = useState('');
+
+  const notify = (isSuccess) => {
+    if (isSuccess === true)
+      toast('Message received! I will reach out to you soon.');
+    else toast('Message could not be sent! Please try again.');
+  };
 
   const [formDetails, setFormDetails] = useState({
     name: '',
@@ -116,6 +123,8 @@ const ContactRight = () => {
         process.env.REACT_APP_VISITOR_MAIL_USER_ID
       );
 
+      notify(true);
+
       setFormDetails({
         name: '',
         phone: '',
@@ -124,161 +133,178 @@ const ContactRight = () => {
         message: '',
       });
     } catch (error) {
+      notify(false);
       console.error('Failed to send email', error.text);
     }
   };
 
   return (
-    <div className="w-[100%]  my-2 p-3 h-auto shadow-shadowOne text-[#9CA3AF]">
-      <form
-        className=" py-2 md:py-4"
-        onSubmit={sendMail}
-      >
-        <div className="flex flex-col items-center justify-center gap-4 w-full ">
-          <div className="w-full flex flex-col gap-4 md:flex-row md:justify-between px-4 py-2 ">
-            <div className="flex flex-col items-center justify-center gap-2 w-[100%] md:w-[48%]">
-              <div className="w-full">
-                <label className="ml-2">Your Name</label>
+    <>
+      <div className="w-[100%]  my-2 p-3 h-auto shadow-shadowOne text-[#9CA3AF]">
+        <form className=" py-2 md:py-4" onSubmit={sendMail}>
+          <div className="flex flex-col items-center justify-center gap-4 w-full ">
+            <div className="w-full flex flex-col gap-4 md:flex-row md:justify-between px-4 py-2 ">
+              <div className="flex flex-col items-center justify-center gap-2 w-[100%] md:w-[48%]">
+                <div className="w-full">
+                  <label className="ml-2">Your Name</label>
+                </div>
+                <div className="w-full">
+                  <input
+                    id="name"
+                    type="text"
+                    className="w-full px-4 py-2 md:p-4  bg-[#191B1E]  rounded-lg highlight"
+                    value={formDetails.name}
+                    onChange={(e) => {
+                      ValidateForm(e.target.value, 'name');
+                    }}
+                  />
+                  <div className="w-full h-[1px]">
+                    {!formValidity.name && (
+                      <p className="text-designColor text-xs text-end -pt-[10px] mr-2 mt-1">
+                        {formErrors.name}
+                      </p>
+                    )}
+                  </div>
+                </div>
               </div>
-              <div className="w-full">
-                <input
-                  id="name"
-                  type="text"
-                  className="w-full px-4 py-2 md:p-4  bg-[#191B1E]  rounded-lg highlight"
-                  value={formDetails.name}
-                  onChange={(e) => {
-                    ValidateForm(e.target.value, 'name');
-                  }}
-                />
-                <div className="w-full h-[1px]">
-                  {!formValidity.name && (
-                    <p className="text-designColor text-xs text-end -pt-[10px] mr-2 mt-1">
-                      {formErrors.name}
-                    </p>
-                  )}
+
+              <div className="flex flex-col items-center justify-center gap-2 w-[100%] md:w-[48%]">
+                <div className="w-full">
+                  <label className="ml-2">Phone Number</label>
+                </div>
+
+                <div className="w-full">
+                  <input
+                    id="phone"
+                    type="text"
+                    value={formDetails.phone}
+                    onChange={(e) => {
+                      ValidateForm(e.target.value, 'phone');
+                    }}
+                    className="w-full px-4 py-2 md:p-4 rounded-md bg-[#191B1E]  highlight "
+                  />
+                  <div className="w-[100%] h-[1px] mt-[2px]">
+                    {!formValidity.phone && (
+                      <p className="text-designColor text-xs text-end -pt-[10px] ">
+                        {formErrors.phone}
+                      </p>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
 
-            <div className="flex flex-col items-center justify-center gap-2 w-[100%] md:w-[48%]">
-              <div className="w-full">
-                <label className="ml-2">Phone Number</label>
-              </div>
-
-              <div className="w-full">
-                <input
-                  id="phone"
-                  type="text"
-                  value={formDetails.phone}
-                  onChange={(e) => {
-                    ValidateForm(e.target.value, 'phone');
-                  }}
-                  className="w-full px-4 py-2 md:p-4 rounded-md bg-[#191B1E]  highlight "
-                />
-                <div className="w-[100%] h-[1px] mt-[2px]">
-                  {!formValidity.phone && (
-                    <p className="text-designColor text-xs text-end -pt-[10px] ">
-                      {formErrors.phone}
-                    </p>
-                  )}
+            <div className="w-full px-4">
+              <div className="flex flex-col items-center justify-center gap-2 w-full">
+                <div className="w-full">
+                  <label className="ml-2">Email</label>
+                </div>
+                <div className="w-full">
+                  <input
+                    id="email"
+                    type="text"
+                    value={formDetails.email}
+                    onChange={(e) => {
+                      ValidateForm(e.target.value, 'email');
+                    }}
+                    className="w-full px-4 py-2 md:p-4 rounded-md bg-[#191B1E]  highlight"
+                  />
+                  <div className="w-[100%] h-[1px] mt-[2px]">
+                    {!formValidity.email && (
+                      <p className="text-designColor text-xs text-end -pt-[10px] ">
+                        {formErrors.email}
+                      </p>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <div className="w-full px-4">
-            <div className="flex flex-col items-center justify-center gap-2 w-full">
-              <div className="w-full">
-                <label className="ml-2">Email</label>
-              </div>
-              <div className="w-full">
-                <input
-                  id="email"
-                  type="text"
-                  value={formDetails.email}
-                  onChange={(e) => {
-                    ValidateForm(e.target.value, 'email');
-                  }}
-                  className="w-full px-4 py-2 md:p-4 rounded-md bg-[#191B1E]  highlight"
-                />
-                <div className="w-[100%] h-[1px] mt-[2px]">
-                  {!formValidity.email && (
-                    <p className="text-designColor text-xs text-end -pt-[10px] ">
-                      {formErrors.email}
-                    </p>
-                  )}
+            <div className="w-full px-4">
+              <div className="flex flex-col items-center justify-center gap-2 w-full">
+                <div className="w-full">
+                  <label className="ml-2">Subject</label>
+                </div>
+                <div className="w-full">
+                  <input
+                    id="subject"
+                    type="text"
+                    value={formDetails.subject}
+                    onChange={(e) => {
+                      ValidateForm(e.target.value, 'subject');
+                    }}
+                    className="w-full px-4 py-2 md:p-4 rounded-md bg-[#191B1E] highlight "
+                  />
+                  <div className="w-[100%] h-[1px] mt-[2px]">
+                    {!formValidity.subject && (
+                      <p className="text-designColor text-xs text-end -pt-[10px] ">
+                        {formErrors.subject}
+                      </p>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <div className="w-full px-4">
-            <div className="flex flex-col items-center justify-center gap-2 w-full">
-              <div className="w-full">
-                <label className="ml-2">Subject</label>
-              </div>
-              <div className="w-full">
-                <input
-                  id="subject"
-                  type="text"
-                  value={formDetails.subject}
-                  onChange={(e) => {
-                    ValidateForm(e.target.value, 'subject');
-                  }}
-                  className="w-full px-4 py-2 md:p-4 rounded-md bg-[#191B1E] highlight "
-                />
-                <div className="w-[100%] h-[1px] mt-[2px]">
-                  {!formValidity.subject && (
-                    <p className="text-designColor text-xs text-end -pt-[10px] ">
-                      {formErrors.subject}
-                    </p>
-                  )}
+            <div className="w-full px-4">
+              <div className="flex flex-col items-center justify-center gap-2 w-full">
+                <div className="w-full">
+                  <label className="ml-2">Message</label>
+                </div>
+
+                <div className="w-full resize-none">
+                  <textarea
+                    id="your_message"
+                    type="text"
+                    value={formDetails.message}
+                    onChange={(e) => {
+                      ValidateForm(e.target.value, 'message');
+                    }}
+                    className="w-full h-[150px] px-4 py-2 md:h-[250px]  md:p-4 rounded-md   overflow-auto resize-none highlight bg-[#191B1E]"
+                  />
+                  <div className="w-[100%] h-[1px] mt-[2px]">
+                    {!formValidity.message && (
+                      <p className="text-designColor text-xs text-end -pt-[10px] mr-2 mt-1">
+                        {formErrors.message}
+                      </p>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <div className="w-full px-4">
-            <div className="flex flex-col items-center justify-center gap-2 w-full">
-              <div className="w-full">
-                <label className="ml-2">Message</label>
-              </div>
-
-              <div className="w-full resize-none">
-                <textarea
-                  id="your_message"
-                  type="text"
-                  value={formDetails.message}
-                  onChange={(e) => {
-                    ValidateForm(e.target.value, 'message');
-                  }}
-                  className="w-full h-[150px] px-4 py-2 md:h-[250px]  md:p-4 rounded-md   overflow-auto resize-none highlight bg-[#191B1E]"
-                />
-                <div className="w-[100%] h-[1px] mt-[2px]">
-                  {!formValidity.message && (
-                    <p className="text-designColor text-xs text-end -pt-[10px] mr-2 mt-1">
-                      {formErrors.message}
-                    </p>
-                  )}
-                </div>
-              </div>
+            <div className="w-full  px-4 py-2 flex flex-col gap-4">
+              <button
+                type="submit"
+                className="w-full p-4 text-white rounded-md bg-[#141518] bg-opacity-50  highlight hover:text-blue-700 hover:bg-gray-900"
+              >
+                Submit
+              </button>
+              {finalFormError?.length > 0 && (
+                <p className="text-designColor text-sm py-1">
+                  {finalFormError}
+                </p>
+              )}
             </div>
           </div>
+        </form>
+      </div>
 
-          <div className="w-full  px-4 py-2 flex flex-col gap-4">
-            <button
-              type="submit"
-              className="w-full p-4 text-white  rounded-md bg-[#141518]  highlight"
-            >
-              Submit
-            </button>
-            {finalFormError?.length > 0 && (
-              <p className="text-designColor text-sm py-1">{finalFormError}</p>
-            )}
-          </div>
-        </div>
-      </form>
-    </div>
+      <div>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+      </div>
+    </>
   );
 };
 
