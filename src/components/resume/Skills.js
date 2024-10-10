7e9f6655a21ee246e0ee35ae16f1acdfa1a920c7
OptimizationGuru <dev.shivamtiwari@gmail.com>
2024-10-10 12:36:12 +0530

    Skills

--- src/components/resume/Skills.js
+++ src/components/resume/Skills.js
@@ -37,7 +37,7 @@ const Skills = () => {
         </div>
 
         <div className="my-4 lg:my-7 w-full">
-          <p className="text-sm  font-medium">State Management/Redux</p>
+          <p className="text-sm  font-medium">State Management/Redux Toolkit</p>
           <span className="w-full h-2 mt-2 bg-black bg-opacity-40 inline-flex rounded-lg ">
             <span className="w-[90%] h-full bg-gradient-to-r rounded-lg from-blue-700 via-pink-500 to-red-600 relative">
               <span className="absolute  -top-7 right-0">90%</span>
