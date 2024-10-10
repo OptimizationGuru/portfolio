06f357112a995c62821cc5f062dababb5efdc2f7
Shivam Tiwari <shivam.tiwari@yuma.energy>
2024-10-10 11:51:02 +0530

    bugfix

--- src/components/resume/Skills.js
+++ src/components/resume/Skills.js
@@ -19,7 +19,7 @@ const Skills = () => {
         </div>
 
         <div className="my-4 lg:my-7 w-full">
-          <p className="text-sm uppercase font-medium">JavaScript</p>
+          <p className="text-sm uppercase font-medium">TypeScript | JavaScript</p>
           <span className="w-full h-2 mt-2 bg-black bg-opacity-40 inline-flex rounded-lg ">
             <span className="w-[90%] h-full bg-gradient-to-r rounded-lg from-blue-700 via-pink-500 to-red-600 relative">
               <span className="absolute  -top-7 right-0">90%</span>
@@ -63,7 +63,7 @@ const Skills = () => {
         </div>
 
         <div className="my-4 lg:my-7 w-full">
-          <p className="text-sm  font-medium">VueJS</p>
+          <p className="text-sm  font-medium">Next.js</p>
           <span className="w-full h-2 mt-2 bg-black bg-opacity-40 inline-flex rounded-lg ">
             <span className="w-[80%] h-full bg-gradient-to-r rounded-lg from-blue-700 via-pink-500 to-red-600 relative">
               <span className="absolute  -top-7 right-0">80%</span>
