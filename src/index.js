82b92ff12b36c3eb10ed119b6452635a19fe290e
OptimizationGuru <dev.shivamtiwari@gmail.com>
2024-10-13 00:04:39 +0530

    ga added

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ReactGA from 'react-ga';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+ReactGA.initialize(process.env.REACT_APP_GA_ID);
+
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 root.render(
   <React.StrictMode>
     <App />
